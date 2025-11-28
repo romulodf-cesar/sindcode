e675918b12846bfb9ae41a5584df586c51779002
romulodf-cesar <romulodf.cesar@gmail.com>
2025-11-28 08:59:39 -0300

    avaliação 01

--- associados/urls.py
+++ associados/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from associados.views import associados
+from associados.views import associados,login,cadastro
 urlpatterns = [
     path('associados',associados, name='associados'),
+    path('login',login, name='login'),
+    path('cadastro',cadastro, name='cadastro'),
 
 ]
--- associados/views.py
+++ associados/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def associados(request):
     return render(request, 'associados/index.html')
+def login(request):
+    return render(request, 'associados/login.html')
+def cadastro(request):
+    return render(request, 'associados/cadastro.html')
