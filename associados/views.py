b0626e38073748680fb8afd9138958efb44e0d6d
romulodf-cesar <romulodf.cesar@gmail.com>
2025-11-28 10:25:05 -0300

    criei o formulário com CSRF

--- associados/views.py
+++ associados/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-
+from associados.forms import AssociadoForm
 def associados(request):
     return render(request, 'associados/index.html')
 def login(request):
     return render(request, 'associados/login.html')
 def cadastro(request):
-    return render(request, 'associados/cadastro.html')
+    form = AssociadoForm()
+    return render(request, 'associados/cadastro.html',{'form': form})
