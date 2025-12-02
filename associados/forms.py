06a2349c900d450bf3a24cfbda900fc8de7ab618
romulodf-cesar <romulodf.cesar@gmail.com>
2025-12-02 10:28:52 -0300

    encerrando aula 7

--- associados/forms.py
+++ associados/forms.py
@@ -32,9 +32,51 @@ class AssociadoForm(forms.Form):
         required=True,
         help_text='Use apenas se a opção "Outro" tiver sido selecionada.'
     )
-    senha = forms.CharField(
-        label="Senha",
+    email = forms.EmailField(
+        label="E-mail",
+        required=True,
+        max_length=100,
+        widget=forms.EmailInput(attrs={'class': 'form-control',
+                                       'placeholder': 'Digite seu e-mail'
+                                       }),
+
+
+    )
+    senha_1 = forms.CharField(
+        label='Senha',
         required=True,
         max_length=70,
-        widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite sua senha',
+            }
+        ),
     )
+
+    senha_2 = forms.CharField(
+        label='Senha',
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Confirme sua senha',
+            }
+        ),
+    )
+
+class LoginForms(forms.Form):
+    nome_login = forms.CharField(
+            label='Nome de Login',
+            required=True,
+            max_length=100
+        )
+
+    senha = forms.CharField(
+        label='Senha',
+        required=True,
+        max_length=70,
+        widget = forms.PasswordInput()
+    )
+
--- associados/views.py
+++ associados/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render
-from associados.forms import AssociadoForm
+from django.shortcuts import render, redirect
+from associados.forms import AssociadoForm, LoginForms
+
+
 def associados(request):
     return render(request, 'associados/index.html')
 def login(request):
-    return render(request, 'associados/login.html')
+    form = LoginForms()
+    return render(request, 'associados/login.html', {'form': form})
 def cadastro(request):
     form = AssociadoForm()
+    if request.method == 'POST':
+        form = AssociadoForm(request.POST)
+        if form['senha_1'].value() != form['senha_2'].value():
+            return redirect('cadastro')
     return render(request, 'associados/cadastro.html',{'form': form})
