b3ab19cbe26679affc78cf561017972ddb2fb7c9
romulodf-cesar <romulodf.cesar@gmail.com>
2025-12-02 11:01:39 -0300

    tela de login redirecionada

--- associados/views.py
+++ associados/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from associados.forms import AssociadoForm, LoginForms
+from django.contrib.auth.models import User
 
 
 def associados(request):
@@ -11,6 +12,24 @@ def cadastro(request):
     form = AssociadoForm()
     if request.method == 'POST':
         form = AssociadoForm(request.POST)
-        if form['senha_1'].value() != form['senha_2'].value():
-            return redirect('cadastro')
+        if form.is_valid():
+            if form['senha_1'].value() != form['senha_2'].value():
+                 return redirect('cadastro')
+            nome_completo = form['nome_completo'].value()
+            nome_social = form['nome_social'].value()
+            identidade_genero = form['identidade_genero'].value()
+            email = form['email'].value()
+            senha = form['senha_1'].value()
+            if User.objects.filter(username=nome_completo).exists():
+                return redirect('cadastro')
+            # criar um novo associado
+            associado = User.objects.create_user(
+                username = nome_completo,
+                email = email,
+                password=senha
+            )
+            associado.save()
+            return redirect('login')
+
+
     return render(request, 'associados/cadastro.html',{'form': form})
