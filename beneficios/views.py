8b48a9aace4dce078a6738f881a0d41d1348ce28
romulodf-cesar <romulodf.cesar@gmail.com>
2025-12-09 11:08:00 -0300

    encerrando topico 3 slide

--- beneficios/views.py
+++ beneficios/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,get_object_or_404,redirect
+from django.contrib import messages
 def beneficios(request):
+    if not request.user.is_authenticated:
+        messages.error(request,'Usuário não logado')
+        return redirect('login')
     return render(request, 'beneficios/beneficios.html')
